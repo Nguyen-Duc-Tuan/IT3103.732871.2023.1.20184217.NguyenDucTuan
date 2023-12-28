a969583db97814b14ce8d121d19e9f32890f3e18
Nguyen-Duc-Tuan <83194648+Nguyen-Duc-Tuan@users.noreply.github.com>
2023-12-28 23:17:42 +0700

    Delete lap05 directory

--- lap05/OtherProject/src/lab01/ChoosingOption.java
+++
@@ -1,8 +0,0 @@
-package lab01;
-import javax.swing.JOptionPane;
-public class ChoosingOption {
-	public static void main(String[] args) {
-		int option = JOptionPane.showConfirmDialog(null, "Do you want to change first class ticket ");
-		JOptionPane.showMessageDialog(null, "You have been choosen: " + (option == JOptionPane.YES_OPTION?"YES":"NO"));
-	}
-}
--- lap05/OtherProject/src/lab01/FirstDialog.java
+++
@@ -1,9 +0,0 @@
-package lab01;
-import javax.swing.JOptionPane;
-
-public class FirstDialog {
-	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null,"Hello Dat. How are u today?");
-		System.exit(3);
-	}
-}
